da051917a6f29825fb6057d0d7aa43f38cf2478a
MaulikSon <156066621+MaulikSon@users.noreply.github.com>
2024-01-17 14:32:42 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,60 @@
+
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x392AFA9", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6"); // lobby ban fix 10 years 
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x392BE98", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6"); // ban fix 10 years
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0xA8B25BC", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x4D485E8", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x4FB40B0", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x505B028", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x53CA800", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x383C520", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x389E96D", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x394E424 ", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x9BC50F8 ", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x6FE43E8", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x733857C", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x5E220A0", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x5579400", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libUE4.so" , "0x8776D70", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+
+
+
+
+
+
+
+
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x347B80", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0xEDD68", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0xEDDF8 ", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0xEDE30 ", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0xF5724 ", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0xF5790 ", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0xF57DC", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+
+
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x34E390", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x34F070", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x374024", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x373ED8", (void *) "/x00/x00/x80/xD2/xC0/x03/x5F/xD6");
+
+
+
+
+
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x13F398", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x389EC0", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x389EC0", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x13EECC", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x15E9CC", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x327E28", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x327FFC", (void *) "/x3F/x00/x00/x71"); 
+patch_lib_no_backup("com.pubg.imobile" , "libanogs.so" , "0x348BA0", (void *) "/x3F/x00/x00/x71");
+
+
+
+
+
+
+
+patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0x1301F0", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0x12BE30", (void *) "/x3F/x00/x00/x71");
+patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0x12BBF0", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xC0D0C", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB95AC", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB5404 ", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB4FFC ", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB2584", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB3A5 ", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB3CF", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xB3EB", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0xFFA5", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0x10077", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0x10092 ", (void *) "/x3F/x00/x00/x71"); patch_lib_no_backup("com.pubg.imobile" , "libhdmpve.so" , "0x42E400", (void *) "/x3F/x00/x00/x71");
